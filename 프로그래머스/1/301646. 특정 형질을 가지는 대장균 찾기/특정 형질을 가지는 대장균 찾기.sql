102882f3abe40d4b928cef9f5c6162c511b3e247
moriroKim <162679625+moriroKim@users.noreply.github.com>
2025-03-20 19:44:28 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -5,7 +5,13 @@
 -- (2번 형질을 포함하지 X) AND 1번 OR 3번 OR 1번, 3번
 -- => (0010, 1110, 1010, 0011, 0111) AND (0001 OR 0100 OR 0101)
 -- => (2, 14, 10, 3, 11)
+# SELECT COUNT(ID) AS COUNT
+# FROM ECOLI_DATA
+# WHERE (GENOTYPE & 2) = 0  -- 2번 형질이 없는 경우
+# AND ((GENOTYPE & 1) > 0 OR (GENOTYPE & 3) > 0 OR (GENOTYPE & 4) > 0);  -- 1번 또는 3번 형질 포함
+
+-- 좀더 단순화 시키면..
 SELECT COUNT(ID) AS COUNT
 FROM ECOLI_DATA
 WHERE (GENOTYPE & 2) = 0  -- 2번 형질이 없는 경우
-AND ((GENOTYPE & 1) > 0 OR (GENOTYPE & 3) > 0 OR (GENOTYPE & 4) > 0);  -- 1번 또는 3번 형질 포함
+AND GENOTYPE & 5 > 0
